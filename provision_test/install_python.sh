f84393cbbd737afe5973ee5588407aca88108f5d
Vadym <vtraida@gmail.com>
2018-04-15 23:04:45 +0300

    changes

--- provision_test/install_python.sh
+++ provision_test/install_python.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-#exirtapt-get update
-#apt-get install -y python
+exirtapt-get update
+apt-get install -y python
 sudo cat /host_files/keys/public/vtraida-18key.pub > ~/.ssh/authorized_keys
